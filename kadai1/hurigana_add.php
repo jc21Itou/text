845aed03761faab78d870cab3e22333226923275
jc21Itou <220171@jc21.jp>
2022-11-22 15:06:12 +0900

    abc

---
+++ kadai1/hurigana_add.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>個人情報管理</title>
+
+    </head>
+    <body>
+        ふりがな追加<br />
+        <form method="post" action="yuubinbanngou_add.php">
+            ふりがなを入力してください。<br />
+            <input type="text" name="ID" style="width:200px"><br />
+            <input type="button" onclick="history.back()" value="戻る">
+            <input type="submit" value="OK">
+        </form>
+
+    </body>
+</html>
