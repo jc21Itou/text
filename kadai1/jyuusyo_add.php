81157b3cda0b53cafa7e8b7ae6df04d281b071c3
jc21Itou <220171@jc-21.jp>
2022-11-29 13:45:37 +0900

    2

---
+++ kadai1/jyuusyo_add.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>個人情報管理</title>
+
+    </head>
+    <body>
+        住所追加<br />
+        <form method="post" action="simei_add.php">
+            氏名を入力してください。<br />
+            <input type="text" name="ID" style="width:200px"><br />
+            <input type="button" onclick="history.back()" value="戻る">
+            <input type="submit" value="OK">
+        </form>
+
+    </body>
+</html>
